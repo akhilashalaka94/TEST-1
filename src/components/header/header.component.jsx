8105f281fae0179a3a798aead27f48d1e7b795f7
Akhila Shalaka <97923680+akhilashalaka94@users.noreply.github.com>
2022-02-09 20:53:45 +1100

    Create header.component.jsx

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./header.styles.scss";
+
+const CustomHeader = ({ header }) => {
+  return <div className={"header"}>{header}</div>;
+};
+
+export default CustomHeader;
